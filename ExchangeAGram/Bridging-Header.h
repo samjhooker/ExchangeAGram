928885a08493ad3785d85b1bdebb15b913d1d9b3
samjhooker <samjhooker@gmail.com>
2014-12-08 11:35:37 +1300

    bridging header and full sdk ready to go

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Samuel Hooker on 8/12/14.
+//  Copyright (c) 2014 Jocus Interactive. All rights reserved.
+//
+
+
+
+//Bridginge header allows the use of obj c api within swift application
+
+#import <FacebookSDK/FacebookSDK.h>
